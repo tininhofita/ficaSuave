a8ed676db037f8fda76ef79b1489dbb8d41b59ba
Tininho Fita <tinofranciel@gmail.com>
2025-09-24 20:01:15 -0300

    alterando usuario do banco

--- app/config/db_config.php
+++ app/config/db_config.php
@@ -7,7 +7,7 @@ function getDatabase()
     if ($isProduction) {
         // Configurações para PRODUÇÃO (HostGator)
         $host = 'localhost';
-        $username = 'tininhofita';  // Ajustar conforme necessário
+        $username = 'tininh93_tininhofita';  // Ajustar conforme necessário
         $password = 'Tino7227!7804@'; // Ajustar conforme necessário
         $dbname = 'tininh93_ficasuave';   // Ajustar conforme necessário
     } else {
